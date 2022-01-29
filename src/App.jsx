dd18a6d23db2676054622448fe8bf97c7df91be3
Jean Meira <jean.meira10@hotmail.com>
2022-01-29 18:19:37 -0300

    feat: test statefull/class component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import './App.css';
 
+// # my components
+import Main from './components/main';
+
 export default function App() {
-  return (
-    <div className="App">
-      <h1>Hello world</h1>
-    </div>
-  );
+  return <Main />;
 }
---
+++ src/components/main.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+export default class Main extends Component {
+  // Componentes de classe precisam do método render
+  // Para devolverem os elementos xml, ou seja, renderizar algo
+
+  render() {
+    return <h1>Meu componente de classe com estado</h1>;
+  }
+}
