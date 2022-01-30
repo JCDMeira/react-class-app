85b50233e300fe21bf36ddf20e15af6d6869877a
Jean Meira <jean.meira10@hotmail.com>
2022-01-30 16:34:28 -0300

    refactor: state with class field and arrow func.

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,28 +1,39 @@
 import React, { Component } from 'react';
+import './main.css';
 
 export default class Main extends Component {
+  state = {
+    novaTarefa: '',
+  };
+
+  handleChange = (e) => {
+    this.setState({
+      novaTarefa: e.target.value,
+    });
+  };
+
   // o construtor que recebe as props
-  constructor(props) {
-    super(props);
+  // constructor(props) {
+  //   super(props);
 
-    this.state = {
-      novaTarefa: '',
-    };
+  //   this.state = {
+  //     novaTarefa: '',
+  //   };
 
-    this.inputMudou = this.inputMudou.bind(this);
-    /*
+  //   this.inputMudou = this.inputMudou.bind(this);
+  /*
       _ bind significa vincular, quando se cria uma função ela tem seu próprio
       _ escopo, o que faz o this ser apontado para esse escopo. Então se for usar
       _ funções que mudem o escopo se deve fazer o bind do this, para não perder
       _ a referência do mesmo. Então o this virá a mesma classe do component
     */
-  }
+  // }
 
-  inputMudou(e) {
-    this.setState({
-      novaTarefa: e.target.value,
-    });
-  }
+  // inputMudou(e) {
+  //   this.setState({
+  //     novaTarefa: e.target.value,
+  //   });
+  // }
 
   // Componentes de classe precisam do método render
   // Para devolverem os elementos xml, ou seja, renderizar algo
@@ -33,10 +44,9 @@ export default class Main extends Component {
       <div className="main">
         {/* <h1>Meu componente de classe com estado</h1> */}
         <h1>Lista de tarefas</h1>
-        <p>olhe: {novaTarefa}</p>
 
         <form action="#">
-          <input onChange={this.inputMudou} type="text" />
+          <input onChange={this.handleChange} type="text" />
           <button type="submit">Enviar</button>
         </form>
       </div>
